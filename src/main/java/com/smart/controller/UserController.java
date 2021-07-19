5035f27e0590a441bd84e5c4e167b7ccd5519289
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-19 10:52:10 +0530

    all the user dashboard is designed completely

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,15 +1,31 @@
 package com.smart.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.entity.User;
+import com.smart.service.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+	
 	@RequestMapping("/index")
-	public String userDashboard() {
-		return "index";
+	public String userDashboard(Model model, Principal principal) {
+		String name = principal.getName();
+		User user = userService.getUser(name);
+		
+		model.addAttribute("user", user);
+		model.addAttribute("title", "Hello, " +user.getUserName() + " | smart contact manager");
+		
+		return "normal/user_dashboard";
 	}
 	
 }
