ef490b728f43d6518746b596b3b91c57b51f3439
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-20 09:11:20 +0530

    form for adding contact is added

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -5,8 +5,11 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.smart.entity.Contact;
 import com.smart.entity.User;
 import com.smart.service.UserService;
 
@@ -17,15 +20,33 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping("/index")
-	public String userDashboard(Model model, Principal principal) {
+	// method for adding common data to the view.
+	
+	@ModelAttribute
+	public void addCommonData(Model model, Principal principal) {
+		
 		String name = principal.getName();
 		User user = userService.getUser(name);
-		
-		model.addAttribute("user", user);
 		model.addAttribute("title", "Hello, " +user.getUserName() + " | smart contact manager");
-		
+		model.addAttribute("user", user);
+	}
+	
+	// method for the user dashboard 
+	
+	@GetMapping("/index")
+	public String userDashboard(Model model, Principal principal) {
+
 		return "normal/user_dashboard";
 	}
+	
+	//method for adding contacts handler
+	
+	@GetMapping("/add-contact")
+	public String addContactForm(Model model) {
+		model.addAttribute("contact", new Contact());
+		model.addAttribute("title", " add-contact | smart contact manager");
+		return "normal/add_contact_form";
+	}
+	
 	
 }
