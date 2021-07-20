85ee95bbc49bb0c2f5c4ae1c8121cafeeec465d0
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-20 06:49:39 +0530

    sidebar is attached with the sidebar

--- src/main/resources/static/js/myjs.js
+++ src/main/resources/static/js/myjs.js
@@ -1,2 +1,24 @@
+console.log("hello world...");
 
- console.log("hello world...");
+function toggle()
+{
+
+    if($(".sidebar").is(":visible")){
+
+        //true blockk is here.
+
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left", "0%");
+
+
+    }else{
+
+        //false block is here.
+
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left", "20%");
+
+    }
+
+
+}
